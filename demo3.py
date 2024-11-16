76b3e5ab3fcaac73c33d3355f925f0e66414b4b0
zhengt666 <49064310@qq.com>
2024-11-16 18:20:22 +0800

    demo3 commit

--- demo3.py
+++ demo3.py
@@ -1,6 +1,7 @@
 import numpy as np
 from itertools import combinations
 from project_cash_flows import ProjectCashFlows
+from typing import List
 
 # 定义项目的现金流数据和周期
 project1_cash_flows = [-1000, 300, 400, 500, 600]
@@ -16,22 +17,19 @@ project_list = [project1,project2,project3]
 # 固定的总投资额
 total_investment = 2500
 
+combinations_list = []
+
 # 计算组合的总收益
-def calculate_return(combination):
+def calculate_return(project_list: List[ProjectCashFlows]):
     total_return = 0
     total_invest = 0
-    for project in combination:
-        total_return += project['return']
-        total_invest += project['investment']
+    for project in project_list:
+        combinations_list.append(project)
+        total_return += project.project_invest
+        total_invest += project.project_npv
     return total_return if total_invest <= total_investment else 0
 
-# 生成所有可能的组合
-combinations_list = []
-for r in range(1, len(projects) + 1):
-    combinations_list.extend(list(combinations(projects, r)))
-
-# 找到最优组合
-best_combination = max(combinations_list, key=calculate_return)
-
-print(f"Best combination: {best_combination}")
-print(f"Total return of best combination: {calculate_return(best_combination)}")
+def calc_best_combination(project_list: List[ProjectCashFlows]):
+    print(f"Total return of best combination: {calculate_return(project_list)}")
+    print(f"Best combination: {combinations_list}")
+    return combinations_list
